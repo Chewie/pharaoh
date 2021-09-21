86c39f9a5f6ebe0fd7edf907946bd88a04ed7187
Kévin Sztern <sztern.kevin@gmail.com>
2021-09-21 22:43:41 +0200

    Propagate anyhow errors up to main and add context

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use anyhow::{Context, Result};
 
 use clap::{App, Arg};
 
@@ -11,21 +11,22 @@ use gatherer::{yaml::YamlGatherer, Gatherer};
 use printer::Printer;
 use runner::Runner;
 
-pub fn run() -> Result<(), Box<dyn Error>> {
+pub fn run() -> Result<()> {
     let matches = build_args().get_matches();
-
     let search_dir = matches.value_of("search_dir").unwrap_or(".");
 
     let gatherer = YamlGatherer::new(search_dir.to_string());
-
-    let collection = gatherer.gather()?;
-
     let runner = Runner::new();
-    let report = runner.run_all_tests(collection)?;
-
     let printer = Printer::new();
 
-    printer.print_report(&report, &mut std::io::stdout())?;
+    let collection = gatherer.gather().context("Failed to parse YAML files")?;
+    let report = runner
+        .run_all_tests(collection)
+        .context("Failed to run tests")?;
+
+    printer
+        .print_report(&report, &mut std::io::stdout())
+        .context("Failed to write report")?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::error::Error;
+use anyhow::Result;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     pharaoh::run()
 }
